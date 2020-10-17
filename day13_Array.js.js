fb43002e88ae47de33da511a74a60d371cb3c5a5
pratyush38raj <71686587+pratyush38raj@users.noreply.github.com>
2020-10-17 23:05:35 +0530

    Array

---
+++ day13_Array.js.js
@@ -0,0 +1,17 @@
+var emp=[10,20,30,40,50];  
+for (i=0;i<emp.length;i++){  
+  alert(emp[i]);   
+}  
+
+/*.........direct call of Array.............*/
+
+
+var i;  
+var emp = new Array();  
+emp[0] = "pratyush";  
+emp[1] = "ayush";  
+emp[2] = "piyush";  
+  
+for (i=0;i<emp.length;i++){  
+  alert(emp[i] );  
+}  
